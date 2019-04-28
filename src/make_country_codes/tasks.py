2b938f38010c5fa17c68560caf648cb0b6aee1f2
ewheeler <evanmwheeler@gmail.com>
2019-04-28 19:36:20 -0400

    refactor to ensure WrapperTask completion

--- src/make_country_codes/tasks.py
+++ src/make_country_codes/tasks.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import hashlib
 from functools import update_wrapper
 
@@ -17,7 +18,7 @@ from pset_utils.luigi.task import Requirement
 from pset_utils.luigi.task import TargetOutput
 
 
-FILE_SOURCES = {
+REMOTE_FILE_SOURCES = {
     'unterm': 'https://protocol.un.org/dgacm/pls/site.nsf/files/Country%20Names%20UNTERM2/$FILE/UNTERM%20EFSRCA.xlsx',
     'iso4166': 'https://www.currency-iso.org/dam/downloads/lists/list_one.xml',
     'cldr': 'https://raw.githubusercontent.com/unicode-cldr/cldr-localenames-full/master/main/en/territories.json',
@@ -29,38 +30,12 @@ SCRAPE_SOURCES = {
     'm49': 'https://unstats.un.org/unsd/methodology/m49/overview/',
 }
 
-class wraprepr:
-    def __init__(self, new_repr, func):
-        self._repr = new_repr
-        self._func = func
-        update_wrapper(self, func)
-
-    def __call__(self, *args, **kwargs):
-        return self._func(*args, **kwargs)
-
-    def __repr__(self):
-        return self._repr(self._func)
-
 
 class Salt:
     def __get__(self, task, cls):
         if task is None:
             return self
-
-        # Bind self/task in a closure
-        # ...
-        # and wrap with another to override its  __repr__
-        # why? since we cannot hash a file's contents until we have the file,
-        # our descriptor's __repr__ will be returned by `task.salt` and used
-        # as part of `file_pattern` for the temporary file. this would look
-        # like this: data/FileSource-iso4166-<function Salt.__get__.<locals>.<lambda> at 0x117b086a8>
-        # which is a valid but odd looking filename.
-        # however, since our descriptor binds self/task in a closure each time
-        # that `__get__` is executed, the lambda function's hash/identity used by
-        # __repr__ may not be the same, which means that self.output().move()
-        # cannot find a matching temporary filename
-        # TODO should we memoize or cache these instead of wrapping?
-        return wraprepr(lambda x: "<function: Salt>", lambda x: self(task))
+        return get_salt_for_task(task.requires())
 
     def __call__(self, task):
         """Returns the salt (chars of sha256 checksum) of task's output file
@@ -68,14 +43,15 @@ class Salt:
         :returns: first six chars of sha256 hexdigest of contents of `task.output()`
         :rtype: str 
         """
-        checksum = hashlib.sha256()
-        # TODO read and hash in chunks
-        with task.output().open('r') as f:
-            checksum.update(f.read())
-        return checksum.hexdigest()[:6]
+        return get_salt_for_task(task)
 
-    def __repr__(self):
-        return "<function Salt.__get__.<locals>.<lambda>...>"
+
+def get_salt_for_task(task):
+    checksum = hashlib.sha256()
+    # TODO read and hash in chunks
+    with task.output().open('r') as f:
+        checksum.update(f.read())
+    return checksum.hexdigest()[:6]
 
 
 class FileSource(Task):
@@ -83,20 +59,17 @@ class FileSource(Task):
     DATA_ROOT = 'data/'
 
     source = Parameter()
-    salt = Salt()
+    ext = Parameter()
 
-    pattern = '{task.__class__.__name__}-{task.source}-{task.salt}'
+    pattern = '{task.__class__.__name__}-{task.source}{task.ext}'
     output = TargetOutput(file_pattern=pattern, ext='',
                           base_dir=DATA_ROOT,
                           target_class=LocalTarget,
                           format=format.Nop)
 
     def run(self):
-        url = FILE_SOURCES.get(self.source)
-
+        url = REMOTE_FILE_SOURCES.get(self.source)
         luigi_logger.debug(url)
-
-        root, ext = os.path.splitext(url)
 
         with requests.get(url, stream=True) as r:
             r.raise_for_status()
@@ -104,15 +77,32 @@ class FileSource(Task):
                 for chunk in r.iter_content(chunk_size=128):
                     f.write(chunk)
 
-        checksum = hashlib.sha256()
-        with self.output().open('r') as f:
-            checksum.update(f.read())
-        salt = checksum.hexdigest()[:6]
 
-        self.output().move(f'{self.DATA_ROOT}{self.__class__.__name__}-{self.source}-{salt}{ext}')
+class SaltedLocalFile(Task):
+    __version__ = '0.1'
+    DATA_ROOT = 'data/'
+
+    source = Parameter()
+    ext = Parameter()
+    salt = Salt()
+
+    def requires(self):
+        return FileSource(source=self.source, ext=self.ext)
+
+    pattern = '{task.__class__.__name__}-{task.source}-{task.salt}{task.ext}'
+    output = TargetOutput(file_pattern=pattern, ext='',
+                          base_dir=DATA_ROOT,
+                          target_class=LocalTarget,
+                          format=format.Nop)
+
+    def run(self):
+        salt = get_salt_for_task(self.requires())
+        self.requires().output().copy(self.output().path)
 
 
 class FileSources(WrapperTask):
+
     def requires(self):
-        for slug in FILE_SOURCES.keys():
-            yield FileSource(slug)
+        for slug, url in REMOTE_FILE_SOURCES.items():
+            _, ext = os.path.splitext(url)
+            yield SaltedLocalFile(source=slug, ext=ext)
